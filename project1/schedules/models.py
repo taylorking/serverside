b7256b0f73accf67e9ba363c6cd4afef0eb54335
Taylor King <kingtb@appstate.edu>
2015-04-14 02:14:55 +0000

    commiting work

--- project1/schedules/models.py
+++ project1/schedules/models.py
@@ -38,17 +38,20 @@ class Term(models.Model):
             ('SMR', 'Summer'),
             ('FAL', 'Fall')
     )
-    semster = models.CharField(max_length=4, choices=SEMESTERS)
+    semester = models.CharField(max_length=4, choices=SEMESTERS)
     current = models.BooleanField()
-
+    def __str__(self):
+        return str(self.year) + ':' + self.semester
 class Room(models.Model):
     building = models.CharField(max_length=200)
     building_abbreviation = models.CharField(max_length=4)
     room_number = models.CharField(max_length=4)
     capacity = models.IntegerField()
-    day_times_available = models.DateTimeField(auto_now=True) 
+    day_times_available = models.DateTimeField(auto_now=False) 
     course_type = models.CharField(max_length=4,choices=COURSE_TYPES)
     comments = models.CharField(max_length=200)
+    def __str__(self):
+      return self.building_abbreviation + " " + self.room_number
 #    available = JSONField(
 
 class Course(models.Model):
@@ -61,27 +64,46 @@ class Course(models.Model):
     contact_hours = models.IntegerField()
     sections_required = models.IntegerField() 
     scheduling_controlled = models.BooleanField()
+    def __str__(self):
+      return self.discipline + " " + str(self.course_number) + ": " + self.title 
+
+class MeetingAssignment(models.Model):
+    day = models.DateTimeField(auto_now=False)
+    time = models.DateTimeField(auto_now=False)
+    duration = models.FloatField()
+    term = models.ForeignKey('Term')
+    room = models.ForeignKey('Room')
+
+
+class MeetingAssignmentField(models.Field):
+    def __init(self):
+        assign = MeetingAssignment()
+        self.day = assign.day 
+        self.time = assign.time
+        self.duration = assign.duration
+        self.term = assign.term
+        self.room = assign.room
+        super(MeetingAssignmentField, self).__init__()
+    def db_type(self, connection): 
+      return 'MeetingAssignment'
 
 class Section(models.Model):
     term = models.ForeignKey('Term')
     course = models.ForeignKey('Course')
     instructor = models.ForeignKey('Instructor')
     section_number = models.IntegerField()
-    meeting_assignment = models.ManyToManyField('MeetingAssignment')
+    meeting_assignment = MeetingAssignmentField()
+ #   meeting_assignment = models.ManyToManyField('MeetingAssignment')
     type_room_needed = models.CharField(max_length=4,choices=COURSE_TYPES)
     seats_required = models.IntegerField()
-
-class MeetingAssignment(models.Model):
-    day = models.DateTimeField(auto_now=True)
-    time = models.FloatField()
-    duration = models.FloatField()
-    term = models.ForeignKey('Term')
-    room = models.ForeignKey('Room')
-
+    def __str__(self):
+      return self.course + " " + self.meeting_assignment
 
 class Instructor(models.Model):
     title = models.CharField(max_length = 200) 
     name = models.CharField(max_length=200)
     preferences = models.CharField(max_length=200)
     requested_courses = models.ManyToManyField('Course')
+    def __str__(self):
+      return self.name
     # Create your models here.
